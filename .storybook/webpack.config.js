11331b7118d15e3c1be695bf65d3f6239a4fd53b
John Bristowe <john@bristowe.com>
2019-04-23 16:46:18 +1000

    Updated SASS loading

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules.push({
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
     test: /\.scss$/,
-    loaders: ["style-loader", "css-loader", "sass-loader"],
-    include: path.resolve(__dirname, "../")
+    loaders: ['sass-loader'],
+    include: path.resolve(__dirname, '../'),
   });
 
-  return defaultConfig;
+  return config;
 };
