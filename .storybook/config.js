3d125649e5c9f4f61e902c8c38fe9e4d9338c331
John Bristowe <john@bristowe.com>
2019-04-23 16:46:05 +1000

    Removed add-on and cleaned up configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { addDecorator, configure, setAddon } from '@storybook/react';
-import infoAddon, { setDefaults } from '@storybook/addon-info';
-import { setOptions } from '@storybook/addon-options';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import centered from '@storybook/addon-centered';
+import { withKnobs } from '@storybook/addon-knobs';
 import Container from './container';
 
-// addon-info defaults
-setDefaults({
-  maxPropStringLength: 200, // Displays the first 200 characters in the default prop string
-});
-setAddon(infoAddon);
-
-setOptions({
-  name: 'KendoReact'
-});
-
+addDecorator(withInfo);
+addDecorator(withKnobs);
 addDecorator(centered);
 addDecorator(story => <Container story={story} />);
+
+addParameters({
+  options: {
+    name: 'KendoReact'
+  }
+});
 
 configure(function () {
   const req = require.context('../src/stories', true, /story\.js$/);
