81bf2a64b39f2e6a959f7c661dc379b65655940d
John Bristowe <john@bristowe.com>
2019-06-13 07:33:02 +1000

    Adjusted for responsive demo

--- src/stories/Buttons/ButtonGroup/responsive-story.tsx
+++ src/stories/Buttons/ButtonGroup/responsive-story.tsx
@@ -4,13 +4,9 @@ import { Button, ButtonGroup } from '@progress/kendo-react-buttons';
 import props from './props';
 
 storiesOf('Buttons/ButtonGroup', module).add('Responsive', () =>
-  <div className="row" {...props()}>
-    <div className="col-xs-12 col-sm-6 example-col">
-      <ButtonGroup width="100%">
-        <Button togglable={true} icon="bold">Bold</Button>
-        <Button togglable={true} icon="italic">Italic</Button>
-        <Button togglable={true} icon="underline">Underline</Button>
-      </ButtonGroup>
-    </div>
-  </div>
+  <ButtonGroup {...props()} width="100%">
+    <Button togglable={true} icon="bold">Bold</Button>
+    <Button togglable={true} icon="italic">Italic</Button>
+    <Button togglable={true} icon="underline">Underline</Button>
+  </ButtonGroup>
 );
