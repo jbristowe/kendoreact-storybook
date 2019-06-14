775a83a07bd46fa69cfe2678f2a7b0c8ad0b131b
John Bristowe <john@bristowe.com>
2019-06-14 10:20:55 +1000

    Updated all dependencies

--- src/stories/Buttons/SplitButton/props.ts
+++ src/stories/Buttons/SplitButton/props.ts
@@ -18,7 +18,7 @@ const props = () => ({
   onFocus: action('focus'),
   onItemClick: action('itemClick'),
   onOpen: action('open'),
-  tabIndex: number('tabIndex', null, 'SplitButton'),
+  tabIndex: number('tabIndex', null, null, 'SplitButton'),
   text: text('text', 'This is the text...', 'SplitButton'),
   textField: text('textField', null, 'SplitButton')
 });
