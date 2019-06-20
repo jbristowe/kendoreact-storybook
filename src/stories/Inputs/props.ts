c3a1649d4c6e76ef68f9e72307a6d86d0ae00cf7
John Bristowe <john@bristowe.com>
2019-06-21 08:16:15 +1000

    Updated props for inputs stories

--- src/stories/Inputs/props.ts
+++ src/stories/Inputs/props.ts
@@ -1,40 +1,12 @@
-import { action } from '@storybook/addon-actions';
-import { boolean } from '@storybook/addon-knobs';
+import { boolean, text } from '@storybook/addon-knobs';
 
 const props = () => ({
-  // behavior knobs
-  disabled: boolean('disabled', false, 'Behavior'),
-
-  // event actions
-  onAbort: action('abort'),
-  onAbortCapture: action('abortCapture'),
-  onAnimationEnd: action('animationEnd'),
-  onAnimationEndCapture: action('animationEndCapture'),
-  onAnimationIteration: action('animationIteration'),
-  onAnimationIterationCapture: action('animationIterationCapture'),
-  onAnimationStart: action('animationStart'),
-  onAnimationStartCapture: action('animationStartCapture'),
-  onBlur: action('blur'),
-  onBlurCapture: action('blurCapture'),
-  onCanPlay: action('canPlay'),
-  onCanPlayCapture: action('canPlayCapture'),
-  onCanPlayThrough: action('canPlayThrough'),
-  onCanPlayThroughCapture: action('canPlayThroughCapture'),
-  onChange: action('change'),
-  onChangeCapture: action('changeCapture'),
-  onClick: action('click'),
-  onClickCapture: action('clickCapture'),
-  onCompositionEnd: action('compositionEnd'),
-  onCompositionEndCapture: action('compositionEndCapture'),
-  onCompositionStart: action('compositionStart'),
-  onCompositionStartCapture: action('compositionStartCapture'),
-  onCompositionUpdate: action('compositionUpdate'),
-  onCompositionUpdateCapture: action('compositionUpdateCapture'),
-  onContextMenu: action('contextMenu'),
-  onContextMenuCapture: action('contextMenuCapture'),
-  onCopy: action('copy'),
-  onCopyCapture: action('copyCapture'),
-  onFocus: action('focus')
+  defaultValue: text('defaultValue', null),
+  label: text('label', null),
+  valid: boolean('valid', true),
+  validationMessage: text('validationMessage', null),
+  validityStyles: boolean('validityStyles', true),
+  value: text('value', null)
 });
 
 export default props;
