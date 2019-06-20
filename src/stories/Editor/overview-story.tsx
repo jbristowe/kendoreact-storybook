a35b58df4077f2272900afaf075b16080932f2a6
John Bristowe <john@bristowe.com>
2019-06-20 15:49:20 +1000

    Refactored Editor story

--- src/stories/Editor/overview-story.tsx
+++ src/stories/Editor/overview-story.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Editor, EditorTools } from '@progress/kendo-react-editor';
+import { Editor } from '@progress/kendo-react-editor';
 import { text } from '@storybook/addon-knobs';
 import content from './content';
 import props from './props';
 
-const { Bold, Italic, Underline,
-  AlignLeft, AlignRight, AlignCenter,
-  Indent, Outdent,
-  OrderedList, UnorderedList,
-  Undo, Redo, Link, Unlink } = EditorTools;
-
 storiesOf('Editor', module).add('Overview', () =>
-  <Editor
-    {...props()}
-    defaultContent={text('defaultContent', content)}
-    tools={[
-      [ Bold, Italic, Underline ],
-      [ Undo, Redo ],
-      [ Link, Unlink ],
-      [ AlignLeft, AlignCenter, AlignRight ],
-      [ OrderedList, UnorderedList, Indent, Outdent ]
-    ]}
-  />, {
+  <Editor {...props()} defaultContent={text('defaultContent', content)} />, {
     knobs: {
       escapeHTML: false
     }
--- src/stories/Editor/props.ts
+++ src/stories/Editor/props.ts
@@ -1,4 +1,13 @@
+import { EditorTools } from "@progress/kendo-react-editor";
+
+const { Bold, Italic, Underline,
+  AlignLeft, AlignRight, AlignCenter,
+  Indent, Outdent,
+  OrderedList, UnorderedList,
+  Undo, Redo, Link, Unlink } = EditorTools;
+
 export const props = () => ({
+  tools: [ EditorTools.Bold ],
   style: {width:800},
 });
 
