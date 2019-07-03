1efc4ccfa1db52cf6c061d3b3761b1352b42c3c9
John Bristowe <john@bristowe.com>
2019-07-04 09:03:49 +1000

    Updated props to fix build

--- src/stories/Inputs/NumericTextBox-story.tsx
+++ src/stories/Inputs/NumericTextBox-story.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { NumericTextBox } from '@progress/kendo-react-inputs';
-import props from './props';
+import NumericTextBoxProps from './props';
 
 storiesOf('Inputs/NumericTextBox', module).add('Overview', () =>
-  <NumericTextBox {...props()} />
+  <NumericTextBox {...NumericTextBoxProps()} />
 );
--- src/stories/Inputs/props.ts
+++ src/stories/Inputs/props.ts
@@ -1,12 +1,15 @@
 import { boolean, text } from '@storybook/addon-knobs';
 
 const props = () => ({
-  defaultValue: text('defaultValue', null),
+  // defaultValue: text('defaultValue', null),
   label: text('label', null),
   valid: boolean('valid', true),
   validationMessage: text('validationMessage', null),
-  validityStyles: boolean('validityStyles', true),
-  value: text('value', null)
+  validityStyles: boolean('validityStyles', true)
+});
+
+export const NumericTextBoxProps = () => ({
+  ...props()
 });
 
 export default props;
