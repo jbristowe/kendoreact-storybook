e789fa9e648e54f06b7ef9bc120cc8aad790c024
John Bristowe <john@bristowe.com>
2019-09-23 15:54:43 +1000

    Removed Docs add-on

--- .storybook/addons.ts
+++ .storybook/addons.ts
@@ -4,7 +4,6 @@ import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
-import '@storybook/addon-docs/register';
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-viewport/register';
 import 'storybook-addon-i18n/register.js';
